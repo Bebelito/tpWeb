3737841e420d414685fba69affd5b0b3734d60bb
Bebelito <Bebelito@Pexing-Carrot>
2020-12-07 12:17:34 +0100

    added interactions, adding model classes (WIP)

--- interaction.js
+++ interaction.js
@@ -3,10 +3,50 @@
 // L'interacteur viendra dans un second temps donc ne vous en souciez pas au départ.
 function DnD(canvas, interactor) {
 	// Définir ici les attributs de la 'classe'
-
+    this.startx = 0
+    this.starty = 0
+    this.endx = 0
+    this.endy = 0
+    var pressed = false
 	// Developper les 3 fonctions gérant les événements
 
+  this.mouseDown = function (evt) {
+    pressed = true
+    var coord = getMousePosition(canvas, evt)
+    this.startx = coord.x
+    this.starty = coord.y
+    console.log('mouseDown : '+this.startx+', '+this.starty)
+    //interactor.onInteractionUpdate(this)
+  }.bind(this);
+
+  this.mouseMove = function (evt) {
+    if (pressed) {
+      var coord = getMousePosition(canvas, evt)
+      this.endx = coord.x
+      this.endy = coord.y
+      console.log('mouseMove : '+coord.x+', '+coord.y)
+      //interactor.onInteractionUpdate(this)
+    }
+  }.bind(this);
+
+  this.mouseUp = function (evt) {
+    if (pressed) {
+      var coord = getMousePosition(canvas, evt)
+      this.endx = coord.x
+      this.endy = coord.y
+      console.log('mouseMove : '+coord.x+', '+coord.y)
+      pressed = false
+      //interactor.onInteractionUpdate(this)
+    }
+
+  }.bind(this);
+
 	// Associer les fonctions précédentes aux évènements du canvas.
+  canvas.addEventListener('mousedown', this.mouseDown, false)
+  canvas.addEventListener('mouseup', this.mouseUp, false)
+  canvas.addEventListener('mousemove', this.mouseMove, false)
+
+
 };
 
 
@@ -18,6 +58,3 @@ function getMousePosition(canvas, evt) {
     y: evt.clientY - rect.top
   };
 };
-
-
-
--- model.js
+++ model.js
@@ -1,3 +1,45 @@
-
 // Implémenter ici les 4 classes du modèle.
 // N'oubliez pas l'héritage !
+
+function Drawing {
+  this.shapes = [];
+  this.deletedShapes = [];
+
+
+
+  this.add = function (shape) {
+    this.shapes.push(shape)
+  }.bind(this);
+
+  this.remove = function (index) {
+    var toDelete = this.shapes[index]
+    this.shapes.splice(index)
+    this.deletedShapes.push(toDelete)
+  }.bind(this);
+
+  this.undo = function () {
+    this.shapes.push(this.deletedShapes[this.deletedShapes.length-1])
+    this.deletedShapes.splice(this.deletedShapes.length-1)
+  }
+};
+
+this.Shape = function {
+  this.color
+  this.thickness
+};
+
+this.Shape.prototype.Rectangle = function (color, thickness) {
+  Shape.call(this,color, thickness);
+  this.x
+  this.y
+  this.width
+  this.height
+};
+
+this.Shape.prototype.Line = function (x1, y1, x2, y2, color, thickness) {
+  Shape.call(this,color, thickness)
+  this.startx
+  this.starty
+  this.endx
+  this.endy
+};
